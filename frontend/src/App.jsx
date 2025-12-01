e1003e02c5083a87207adba4087e23ff7cfc7eee
simosw69 <14simonecampagna14@gmail.com>
2025-12-01 13:13:07 +0100

    created vite+react frontend project; installed tailwindcss in frontend; installed inertiajs in both symfony and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-center">React + Tailwind + Vite pronto!</h1>
+    </div>
+  );
+}
